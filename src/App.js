b921b98d882ef5940e13f571e06d1a0187909763
Hovhannes <hovhannes.g@sdc.am>
2021-11-03 19:42:39 +0400

    remove unused files

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import Main from "./components/Main/Main";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header/>
       <Main/>
       <Footer/>
-    </div>
+    </>
   );
 }
 

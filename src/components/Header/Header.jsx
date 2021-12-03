247b16c7c9c5bd7c1c06c4331355b41edb0ebc97
Hovhannes <hovhannes.g@sdc.am>
2021-12-03 15:01:40 +0400

    Add: mobile UI, fix styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,55 @@
-import React from 'react';
-import logo from '../../assets/logo.svg'
-import './styles.scss'
+import React, { useEffect, useState } from "react";
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import logo from "../../assets/logo.svg";
+import "./styles.scss";
+import en_us from "../../resources/en-us.json";
+import hy_am from "../../resources/hy-am.json";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: {
+        ...en_us,
+      },
+    },
+    am: {
+      translation: {
+        ...hy_am,
+      },
+    },
+  },
+  lng: "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 const Header = () => {
-    return (
-        <header className="header">
-            <div className="container">
-                <img src={logo} alt="logo" className="logo"/>
-            </div>
-        </header>
-    )
-}
+  const [language, setLanguage] = useState("am");
+  const { t } = useTranslation();
 
-export default Header 
+  useEffect(() => {
+    i18n.changeLanguage(language);
+  }, []);
+
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="logo-container">
+          <img src={logo} alt="logo" className="logo" />
+          <div className="texts">
+            <h1 className="fz36 fw400 title">{t("title")}</h1>
+            <h2 className="fz18 fw600 subtitle">
+              {t("help-us-to-support-our-community")}
+            </h2>
+          </div>
+        </div>
+        <button className="fz16 fw600 c-black400 change-lang">English</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
